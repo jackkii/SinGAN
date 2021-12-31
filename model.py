2822157026ead5c46178b9ea42f1b571cd426df0
unknown <724791227@qq.com>
2021-12-31 13:50:06 +0800

    初步定义D和G网络

---
+++ model.py
@@ -0,0 +1,72 @@
+# model.py
+# 定义G和D网络
+# 待解决问题：for循环生成卷积层的通道数opt.nfc问题
+import torch
+import torch.nn as nn
+
+
+class ConvBlock(nn.Sequential):
+    # ConvBlock = cov(3x3)-BN-LeakyRelu(0.2)
+    def __init__(self, in_channel, out_channel, ker_size, padding, stride):
+        super(ConvBlock, self).__init__()
+        self.add_module('conv', nn.Conv2d(in_channel, out_channel, kernel_size=ker_size, stride=stride, padding=padding)),
+        self.add_module('norm', nn.BatchNorm2d(out_channel)),
+        self.add_module('LeakyRelu', nn.LeakyReLU(0.2, inplace=True))
+
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv2d') != -1:
+            m.weight.data.normal_(0.0, 0.02)
+        elif classname.find('Norm') != -1:
+            m.weight.data.normal_(1.0, 0.02)
+            m.bias.data.fill_(0)
+
+
+class WDiscriminator(nn.Module):
+    # 马尔可夫判别器
+    def __init__(self, opt):
+        super(WDiscriminator, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        # nfc_current = opt.nfc
+        # self.head = ConvBlock(opt.nc_im, nfc_current, opt.ker_size, opt.padd_size, opt.stride)
+        self.head = ConvBlock(opt.nc_im, opt.nfc, opt.ker_size, opt.padd_size, opt.stride)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            # nfc_current = int(opt.nfc/pow(2, (i+1)))
+            # block = ConvBlock(2*nfc_current, nfc_current, opt.ker_size, opt.padd_size, opt.stride)
+            block = ConvBlock(opt.nfc, opt.nfc, opt.ker_size, opt.padd_size, opt.stride)
+            self.body.add_module('block%d'%(i+1), block)
+        # self.tail = nn.Conv2d(nfc_current, 1, kernel_size=opt.ker_size, stride=opt.stride, padding=opt.padd_size)
+        self.tail = nn.Conv2d(opt.nfc, 1, kernel_size=opt.ker_size, stride=opt.stride, padding=opt.padd_size)
+
+    def forward(self, x):
+        x = self.head(x)
+        x = self.body(x)
+        x = self.tail(x)
+        return x
+
+
+class Generator(nn.Module):
+    # 生成器
+    def __init__(self, opt):
+        super(Generator, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        self.head = ConvBlock(opt.nc_im, opt.nfc, opt.ker_size, opt.padd_size, opt.stride)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            block = ConvBlock(opt.nfc, opt.nfc, opt.ker_size, opt.padd_size, opt.stride)
+            self.body.add_module('block%d' % (i + 1), block)
+        self.tail = nn.Sequential(
+            nn.Conv2d(opt.nfc, opt.nc_im, kernel_size=opt.ker_size, stride=opt.stride, padding=opt.padd_size),
+            nn.Tanh()
+        )
+
+    def forward(self, x, y):
+        # x为该层噪声，y为上层生成结果的上采样，两者都经过了padding
+        # 注意这里的x其实已经添加过y了
+        x = self.head(x)  # x经过5层卷积，其长宽会小于y，所以在输出时需要减小y的大小
+        x = self.body(x)
+        x = self.tail(x)
+        ind = int((y.shape[2] - x.shape[2]) / 2)                     # 求出卷积使长边减少的像素数目（宽边与之相同）
+        y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]  # 让y长宽两侧各删去该数目一半的像素，使得x,y尺寸一致
+        return x + y
